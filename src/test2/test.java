e6c44de5ccafec43a5f599a0174f4a6468ff0523
Sandesh <sandesh.phadatare77@gmail.com>
2022-08-10 13:52:31 +0530

    first commit

---
+++ src/test2/test.java
@@ -0,0 +1,11 @@
+package test2;
+
+public class test {
+
+	public static void main(String[] args) {
+		String name = "sandesh is good";
+		StringBuilder srb = new StringBuilder(name);
+		System.out.println(srb.reverse());
+	}
+
+}
